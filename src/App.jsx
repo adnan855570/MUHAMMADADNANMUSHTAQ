a4f5a392034ea671298c9012435e4d0a90b68562
adnan855570 <ktk855570@gmail.com>
2023-10-14 18:53:14 +0500

    another change

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const siteProps = {
   email: "MUHAMMAD.ADNANMUSHTAQ@studentambassadors.com",
   gitHub: "adnan855570",
   instagram: "adnankhan855570",
-  linkedIn: "Muhammmad Adnan Mushtaq",
+  linkedIn: "https://www.linkedin.com/in/muhammad-adnan-mushtaq-9910a719b/",
   medium: "",
   twitter: "",
   youTube: "",
