78c07d3e8eaaa310031dbf110e97a4ccd5911ee9
adnan855570 <ktk855570@gmail.com>
2023-10-14 16:51:56 +0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Adnan Mushta",
+  title: "MERN Stack ,React Native Developer and AI/Ml Enthusiast",
+  email: "MUHAMMAD.ADNANMUSHTAQ@studentambassadors.com",
+  gitHub: "adnan855570",
+  instagram: "adnankhan855570",
+  linkedIn: "Muhammmad Adnan Mushtaq",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/laptop.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Laptop with pen and Phone";
 
 /**
  * Project list
@@ -31,27 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Movie App🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Movie App with React.js",
+    url: "https://adnan855570.github.io/Movie/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Bootcamp Website ",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Bootcamp Website with Html Css and JavaScript",
+    url: "https://adnan855570.github.io/bootcamp/",
   },
   {
-    title: "My Resume Site",
+    title: "Landing Page",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Landing Page with Bootstrap 5",
+    url: "https://adnan855570.github.io/landingpage1/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Ecommerce Fully Responsive website",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+      "Ecommerce fully Responsive website with Html Css and JavaScript",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
